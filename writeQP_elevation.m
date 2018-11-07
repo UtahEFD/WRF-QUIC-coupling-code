2bd3f8a1788fef3a02fd68d2d5d215eed2742ac6
matt84105 <38793923+matt84105@users.noreply.github.com>
2018-11-07 09:51:45 -0700

    Delete writeQP_elevation.m

--- writeQP_elevation.m
+++
@@ -1,9 +0,0 @@
-function writeQP_elevation(ProjectPathInner)
-
-fid = fopen([ProjectPathInner 'QP_elevation.inp'],'w','l');
-
-fwrite(fid,4*3,'int32');
-fwrite(fid,[0,0,0],'real*4');
-fwrite(fid,4*3,'int32');
-
-fclose(fid);
