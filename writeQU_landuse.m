02d3e11ef977e87e224e00dc2aebc79820a71ad3
matt84105 <38793923+matt84105@users.noreply.github.com>
2018-11-07 09:51:53 -0700

    Delete writeQU_landuse.m

--- writeQU_landuse.m
+++
@@ -1,9 +0,0 @@
-function writeQU_landuse(ProjectPathInner)
-
-fid = fopen([ProjectPathInner 'QU_landuse.inp'],'w','l');
-
-fwrite(fid,4*3,'int32');
-fwrite(fid,[0,0,0],'int32');
-fwrite(fid,4*3,'int32');
-
-fclose(fid);
